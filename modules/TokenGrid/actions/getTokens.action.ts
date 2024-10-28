9e67fb87231477cffdaa8fdb8ac6b8b6bbd84189
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-10-28 23:12:16 +0100

    Update getTokens.action.ts

--- modules/TokenGrid/actions/getTokens.action.ts
+++ modules/TokenGrid/actions/getTokens.action.ts
@@ -17,5 +17,5 @@ export async function getTokens(): Promise<string[]> {
 
   // return tokens.json()
 
-  return ['GGqDS6Nc4wh8vZeHkwkk85gunkdXAPnHZKZH1jTTeASk', '6gvWSka7SnJDn4mqV7Ydn4q7AyVNSo2aX4TNGjBg7Cct', 'HmKdGD3mPiGz1BTHtjEvJpLhtvSLhiqmbjVcAZafQ4S7', '43uhykFm8Y9gLvHrWs7r7w1HCKu6vikDi7j394FaSfNz','Em5SwhRenPvmQ6M8oad3iB5K4xeaX1YCVxRdnNAgtFCd','ChTyTncJBo5gR3iReXbXeujrRUCsvVSEjLVzMtpF3x8a','2x1D1WRifYXGLNJ5MC7Bj3TJUruDhVvWdYQ6u75VemsC','Ho9nf7o2sEfimwSfvUDk1XJ8yL7zByHaR4m6uGCzAJWd']
+  return ['F7bjKTh1hVGPuyAaKHwrUTThS3XPSj5LY8kBegKu8Z3H']
 }
