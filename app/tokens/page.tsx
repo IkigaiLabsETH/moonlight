91041a7cfc4eee1a53df5fc5c6810d7414b56532
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-11-27 12:20:07 +0100

    Update page.tsx

--- app/tokens/page.tsx
+++ app/tokens/page.tsx
@@ -8,7 +8,7 @@ export default async function Tokens() {
       <div className="m-auto">
         <div className="p-5 md:p-10 lg:p-15 bg-neutral-900 text-center md:text-left">
           <div className="max-w-screen-2xl m-auto"> 
-            <h1 className="sm:text-2xl md:text-4xl lg:text-8xl">Cult Coins</h1>
+            <h1 className="sm:text-2xl md:text-4xl lg:text-8xl">AI ART COINS WIF JPG</h1>
           </div>
         </div>
         <div className="max-w-screen-2xl m-auto px-8 py-5 md:py-10 lg:py-20">
